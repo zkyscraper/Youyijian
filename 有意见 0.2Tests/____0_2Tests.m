f262b55b30e2738723b249ff5f96eb5f7cec13b2
Shaocong <zkyscraper@gmail.com>
2014-04-28 20:42:51 +0800

    Revert 68dce0a..2ce3b91
    This rolls back to commit 68dce0a2baba5f9a4e66c27221f7633a82f54bb6.

---
+++ 有意见 0.2Tests/____0_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  ____0_2Tests.m
+//  有意见 0.2Tests
+//
+//  Created by Demo Monkeys on 14-3-28.
+//  Copyright (c) 2014年 DM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____0_2Tests : XCTestCase
+
+@end
+
+@implementation ____0_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
